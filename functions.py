0683b7a0723010c4610c47ac27767e57b01b47a0
Matt Weeks <m_weeks@hotmail.co.uk>
2016-02-24 23:42:05 +0000

    Sort and print initial coefficients.

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 def checkSameSolution(coef, coef2):
-    if ((coef[0] == coef2[0]) & (coef[1] == coef2[1]) & (coef[2] == coef2[2]) & (coef[3] == coef2[3]) & (coef[4] == coef2[4]) & (coef[5] == coef2[5])):
+    if ((coef[1] == coef2[1]) & (coef[2] == coef2[2]) & (coef[3] == coef2[3]) & (coef[4] == coef2[4]) & (coef[5] == coef2[5]) & (coef[6] == coef2[6])):
         return True
     return False
 
@@ -34,8 +34,22 @@ def getInterpolation(data, x):
     y = y1 + (((x - x1) * (y2 - y1)) / (x2 - x1))
     return y
 
+def getRandomCoefficient(range):
+    return r
 def getSingleFitness(point, coef):
     return ((getYfromX(point[0], coef) - point[1]) ** 2)
 
 def getYfromX(x, coef):
-    return (coef[0] + (coef[1] * x) + (coef[2] * (x ** 2)) + (coef[3] * (x ** 3)) + (coef[4] * (x ** 4)) + (coef[5] * (x ** 5)))
+    return (coef[1] + (coef[2] * x) + (coef[3] * (x ** 2)) + (coef[4] * (x ** 3)) + (coef[5] * (x ** 4)) + (coef[6] * (x ** 5)))
+
+def outputCoef(coef):
+    print("a: " + str(coef[1]))
+    print("b: " + str(coef[2]))
+    print("c: " + str(coef[3]))
+    print("d: " + str(coef[4]))
+    print("e: " + str(coef[5]))
+    print("f: " + str(coef[6]))
+    print("fit: " + str(coef[0]))
+
+def sortByFitness(coefs):
+    coefs.sort()
--- script.py
+++ script.py
@@ -1,45 +1,60 @@
 # get the data points provided.
 from dataIS import dataPoints, realDataPoints
 from functions import *
+import random
 
-# a test plot
+# get data for plot
+# use test data points
 # data = dataPoints
 
 # use the assessment data points
 data = realDataPoints
 
-# generate coeficients
+# generate initial coeficients
+# ----------------------------
 # interpolate value for f(0) = a
 a = getInterpolation(data, 0)
 
 # interpolate value for f(1) = a + b + c + d + e + f
 coefSum = getInterpolation(data, 1)
-
-coef = [a, 0, 0, 0, 0, 0]
-
-# calcualte fitnesses
-print(getFitness(data, coef))
+initCoef = []
+for i in range(0,  1000):
+    newCoef = [0, a, 0, 0, 0, 0, 0]
+    coefRange = coefSum - a
+    for j in range(0, 4):
+        # assume the max coefficient isn't larger than 1.2 * f(1)
+        newCoef[(j + 2)] = (random.random() * coefRange * 3) - (coefRange * 1.2)
+        coefRange = coefRange - newCoef[(j + 2)]
+    newCoef[6] = coefRange
+    # calculate fitness
+    newCoef[0] = (getFitness(data, newCoef))
+    initCoef.append(newCoef)
 
 # sort by fitness
+sortByFitness(initCoef)
+coef = initCoef[0]
 
 
 # get best result
-
+# ---------------
+for i in range(0, 10000):
+    x = i
 
 # print current best coeficients
-a, b, c, d, e, f = coef[0:6]
+a, b, c, d, e, f = coef[1:7]
 print("Best:")
-print("t: " + str(coefSum))
-print("a: " + str(a))
-print("b: " + str(b))
-print("c: " + str(c))
-print("d: " + str(d))
-print("e: " + str(e))
-print("f: " + str(f))
+outputCoef(initCoef[0])
+print()
+outputCoef(initCoef[1])
+print()
+outputCoef(initCoef[2])
+print()
 
 # calcualte fitnesses
 print("F: " + str(getFitness(data, coef)))
 
 # excel coeficients
-coef = [-0.006, 5000, 70, -65, 1.03, -0.00103]
-print("X: " + str(getFitness(data, coef)))
+coef = [0, -0.006, 5000, 70, -65, 1.03, -0.00103]
+coef[0] = getFitness(data, coef)
+print("X: " + str(coef[0]))
+print("Z: 3.86877553641e+23")
